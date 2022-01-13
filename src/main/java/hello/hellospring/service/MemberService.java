5959f14bd1a21c5fe7f9362f87de93460e191aee
daseulll <nldaseul@gmail.com>
2022-01-13 23:44:03 +0900

    Add MemberServiceTest.java

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -8,9 +8,15 @@ import java.util.List;
 import java.util.Optional;
 
 public class MemberService {
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
 
-    /** 회원 가입 */
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    /**
+     * 회원 가입
+     */
     public Long join(Member member) {
         // 같은 이름이 있는 중복 회원X
         validateDuplicateMember(member);
